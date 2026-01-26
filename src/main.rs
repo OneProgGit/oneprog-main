9dc45981c304742cf7f066237ffda534a38f0c16
OneProg <jade.weirdo@gmail.com>
2026-01-26 22:02:23 +0300

    Fix. Move to rust 1.93.0. Deps updates

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn App() -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
 
-        div { class: "flex flex-col min-h-screen gap-4 mx-2 items-start justify-start",
+        div { class: "flex flex-col min-h-screen gap-4 mx-2 justify-start",
             Navbar {}
             Footer {}
         }
--- src/navbar.rs
+++ src/navbar.rs
@@ -4,7 +4,7 @@ use dioxus::prelude::*;
 pub fn Navbar() -> Element {
     const VERSION: &str = env!("CARGO_PKG_VERSION");
     rsx! {
-        div { class: "flex flex-col flex-1 gap-4 items-start justify-start",
+        div { class: "flex flex-1 flex-col gap-4 items-start justify-start",
             div {
                 class: "navbar bg-base-200 flex flex-col gap-5 items-start justify-start",
                 id: "navbar",
@@ -12,12 +12,12 @@ pub fn Navbar() -> Element {
             }
             div { class: "flex flex-row gap-5 m-2 items-start jusify-start",
                 a {
-                    class: "btn btn-outline btn-primary",
+                    class: "btn btn-outline btn-primary w-auto",
                     href: "https://git.oneprog.org",
                     "Github"
                 }
                 a {
-                    class: "btn btn-primary",
+                    class: "btn btn-primary w-auto",
                     href: "https://contest.oneprog.org",
                     "Контест"
                 }
